dbbdf76c54b56dae17b60333748ff1601a930d3e
DDinkAA <111251262+DDinkAA@users.noreply.github.com>
2022-11-03 12:21:32 +0000

    Deploying to gh-pages from @ basesnel/web-forge-project-01@8f3e14ec91c7af4296eba9a59330f070067a45e8 🚀

---
+++ index.6e4d2930.js
@@ -0,0 +1,2 @@
+document.querySelector(".js-speaker-form").addEventListener("submit",(function(e){e.preventDefault(),new FormData(e.currentTarget).forEach((function(e,t){return console.log("".concat(t,": ").concat(e))})),e.currentTarget.reset()}));
+//# sourceMappingURL=index.6e4d2930.js.map
