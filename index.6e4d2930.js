ad5c10246c9148fac7a855d1c0f02e599450dd5e
Maksym-Palii <110907706+Maksym-Palii@users.noreply.github.com>
2022-11-06 18:37:40 +0000

    Deploying to gh-pages from @ basesnel/web-forge-project-01@7bd41acbdebde0d065db2d7cfabbe5f974ea0353 🚀

--- index.6e4d2930.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".js-speaker-form").addEventListener("submit",(function(e){e.preventDefault(),new FormData(e.currentTarget).forEach((function(e,t){return console.log("".concat(t,": ").concat(e))})),e.currentTarget.reset()}));
-//# sourceMappingURL=index.6e4d2930.js.map
